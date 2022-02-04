4610212686bc21bc6293b8c9839253a1c6581798
Hansu Lee <kintoberry@gmail.com>
2022-02-04 08:00:21 +0300

    solve creative exercise 1.5.14 (complete)

--- ch1-5/src/CreativeExercises/WeightedQuickUnionByHeight.java
+++ ch1-5/src/CreativeExercises/WeightedQuickUnionByHeight.java
@@ -6,17 +6,14 @@ Author: Kinto
 
 public class WeightedQuickUnionByHeight {
     private int[] id;
-    private int[] weight;
     private int[] height;
     private int count;
     public WeightedQuickUnionByHeight(int N) {
         count = N;
-        weight = new int[N];
         height = new int[N];
         id = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
-            weight[i] = 1;
             height[i] = 0;
         }
     }
@@ -27,25 +24,16 @@ public class WeightedQuickUnionByHeight {
         if (pRoot == qRoot) {
             return;
         }
-        if (weight[pRoot] >= weight[qRoot]) {
+        // The height of the combined tree grows by 1 if and only if when the two trees are the same
+        // The height of the combined tree remains the same if one tree is bigger than the other, since
+        // the smaller tree is attached to the root of the bigger tree.
+        if (height[pRoot] < height[qRoot]) {
+            id[pRoot] = qRoot;
+        } else if (height[pRoot] > height[qRoot]) {
             id[qRoot] = pRoot;
-            weight[pRoot] += weight[qRoot];
-            if (weight[pRoot] == weight[qRoot]) {
-                // The height of the combined tree grows by 1 if and only if when the two trees are the same
-                // The height of the combined tree remains the same if one tree is bigger than the other, since
-                // the smaller tree is attached to the root of the bigger tree.
-                height[pRoot]++;
-            }
-        } else {
-            if (height[pRoot] < height[qRoot]) {
-                id[pRoot] = qRoot;
-            } else if (height[pRoot] > height[qRoot]) {
-                id[qRoot] = pRoot;
-            } else if (height[pRoot] ==  height[qRoot]) {
-                id[pRoot] = qRoot;
-                height[qRoot]++;
-            }
-            weight[qRoot] += weight[pRoot];
+        } else if (height[pRoot] ==  height[qRoot]) {
+            id[pRoot] = qRoot;
+            height[qRoot]++;
         }
         count--;
     }
